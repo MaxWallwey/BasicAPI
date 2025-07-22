d0152ab8f9baf74827420da0e03aae431c83e2d8
MaxWallwey <127151268+MaxWallwey@users.noreply.github.com>
2025-07-22 21:25:40 +0100

    Add cassandra

--- src/main.go
+++ src/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"go-kafka-message-broker/types"
+	"net/http"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 // albums slice to seed record album data.
-var albums = []album{
+var albums = []types.Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
@@ -56,7 +48,7 @@ func getAlbumByID(c *gin.Context) {
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	var newAlbum types.Album
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
